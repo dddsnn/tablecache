fbca19eb2a8be58ec2a970271137e61eb8d09e17
Marc Lehmann <marc.lehmann@gmx.de>
2023-10-10 06:35:49 +0200

    Require Python 3.12

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
     description='Simple cache for unwieldily joined relations.',
     long_description_content_type='text/markdown', long_description=readme,
     author="Marc Lehmann", author_email="marc.lehmann@gmx.de",
-    url='https://github.com/dddsnn/tablecache', python_requires='>=3.11',
+    url='https://github.com/dddsnn/tablecache', python_requires='>=3.12',
     install_requires=requirements,
     extras_require={'test': test_requirements,
                     'dev': dev_requirements}, license='AGPL-3.0-or-later')
