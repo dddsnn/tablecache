f56c78ca7ca538d65d21827ac5c32a12ed388d30
Marc Lehmann <marc.lehmann@gmx.de>
2024-01-23 05:35:04 +0000

    Document indexes more

--- tablecache/index.py
+++ tablecache/index.py
@@ -105,6 +105,44 @@ class Adjustment:
 
 
 class Indexes[PrimaryKey](abc.ABC):
+    """
+    A set of indexes used to access storage and DB tables.
+
+    Provides a uniform way to specify a set of records to be queried from
+    either storage or DB tables. This is done with storage_records_spec() and
+    db_records_spec(), respectively.
+
+    Also keeps track of the set of records that are in storage, as opposed to
+    those that are only available via the DB. To this end, the observe()
+    callback is expected to be called by the cache whenever a record is
+    inserted into storage. Calls to covers() then return whether the set of
+    records specified there is present in storage. Finally, adjust() can be
+    used to change which records are in storage by expressing which ones should
+    be. The implementation then returns which records need to be removed from
+    storage and which ones fetched from the DB and inserted in order to attain
+    that state.
+
+    An Indexes contains one or more indexes that can be used to specify sets of
+    records. An index is defined via a score function, which takes a record's
+    attributes as kwargs and returns a numerical score. Records can then be
+    queried as ranges of these scores quickly. An additional recheck predicate
+    can be defined by the index to filter out some potential bycatch. At the
+    very least, an index named primary_key must exist, which maps a record's
+    primary key to a score.
+
+    The access methods {storage,db}_records_spec() and covers() take an index
+    name along with arbitrary args and kwargs, which the specific
+    implementation needs to interpret in a useful way. The same parameters
+    should always represent the same set of records. E.g., a call to
+    storage_records_spec() yields a StorageRecordsSpec to get a set of records
+    from storage, while a call to db_recods_spec should yield a DbRecordsSpec
+    to get the same set of records from the DB.
+
+    The methods involving index state, covers() and adjust(), may not be
+    supported for every index. E.g., an index may only be meant for querying
+    (i.e. support covers()), but not for adjusting the indexes. In that case,
+    these methods raise an UnsupportedIndexOperation.
+    """
     @property
     def index_names(self) -> t.Iterable[str]:
         """Return names of all indexes."""
@@ -113,34 +151,95 @@ class Indexes[PrimaryKey](abc.ABC):
     @property
     @abc.abstractmethod
     def score_functions(self) -> t.Mapping[str, tp.ScoreFunction]:
+        """
+        A mapping of index names to their respective score functions.
+
+        Always contains an entry primary_key.
+
+        Generally, score functions are expected to be called with all
+        attributes of a record as kwargs, but at least for the primary_key
+        index, just the primary (as a kwarg) will do.
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     def primary_key_score(self, primary_key: PrimaryKey) -> numbers.Real:
+        """
+        Calculate the primary key score.
+
+        This functionn takes a primary key itself as an argument, as opposed to
+        the primary_key entry in score_functions, which takes record attributes
+        as kwargs.
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     def storage_records_spec(
         self, index_name: str, *args: t.Any, **kwargs: t.Any
     ) -> StorageRecordsSpec:
+        """
+        Specify records in storage based on an index.
+
+        Returns a StorageRecordsSpec that specifies the set of records in
+        storage that matches the index parameters. args and kwargs are
+        interpreted in a way specific to the index.
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     def db_records_spec(
             self, index_name: str, *args: t.Any, **kwargs: t.Any
     ) -> DbRecordsSpec:
+        """
+        Specify records in the DB based on an index.
+
+        Like storage_records_spec(), but specifies the same set of records in
+        the DB.
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     def adjust(
             self, index_name: str, *args: t.Any, **kwargs: t.Any
     ) -> Adjustment:
+        """
+        Adjust which records are covered by the indexes.
+
+        Takes implementation-specific args and kwargs for one of the indexes
+        that specify the set of records that should be in storage after the
+        adjustment. Returns an Adjustment, which contains a StorageRecordsSpec
+        of records to delete from storage and a DbRecordsSpec of ones to load
+        from the DB in order to attain that state.
+
+        After the call, the indexes assume that the records that were specified
+        to be deleted from storage are no longer covered, and likewise that
+        those specified to be loaded are. Future calls to covers() will reflect
+        that.
+
+        Raises an UnsupportedIndexOperation if adjusting by the given index is
+        not supported.
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     def covers(
             self, index_name: str, *args: t.Any, **kwargs: t.Any) -> bool:
+        """
+        Check whether the specified records are covered by storage.
 
+        Takes implementation-specific args and kwargs that specify a set of
+        records, and returns whether all of them are in storage. This
+        determination is based on previous calls to adjust() and observe().
+
+        Note that a record may also be considered covered if doesn't exist.
+        E.g., say records with primary keys between 0 and 10 were loaded into
+        storage, but none even exists with primary key 5. Then that record is
+        still covered by storage, and the cache doesn't need to go to the DB to
+        check if that record exists.
+
+        Raises an UnsupportedIndexOperation if the given index doesn't support
+        checking coverage. However, the primary_key index always does.
+        """
         raise NotImplementedError
 
     def observe(self, record: tp.Record) -> None:
@@ -148,7 +247,7 @@ class Indexes[PrimaryKey](abc.ABC):
         Observe a record being inserted into storage.
 
         This can be used by the implementation to maintain information on which
-        score intervals actually exist in cache.
+        records exist and update statistics.
         """
 
 
